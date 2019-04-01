c4a6056fdf6773475e196787a8495761949e201f
flashttf <pawansp72@gmail.com>
2019-04-01 18:27:20 +0530

    Datastructur

--- src/com/bridgelabz/datastructures/UnOrderedListTest.java
+++ src/com/bridgelabz/datastructures/UnOrderedListTest.java
@@ -1,23 +1,52 @@
 package com.bridgelabz.datastructures;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+
+
 import java.util.Scanner;
 
+
+
+
+
 public class UnOrderedListTest {
-public static void main(String[] args) {
-	try {
-		Scanner scanner=new Scanner(System.in);
-		boolean isPresent;
-		
-		String fileLocation="/home/admin1/Desktop/UnorderedList.txt";
-		FileReader fileReader=new FileReader(fileLocation);
-		BufferedReader bufferedreader=new BufferedReader(fileReader);
-		String line=bufferedreader.readLine();
-		System.out.println(line);
-	} catch (Exception e) {
-		// TODO: handle exception
-		System.out.println(e);
+	
+@SuppressWarnings("unchecked")
+public static void main(String[] args)
+{	
+	Scanner scan=new Scanner(System.in);
+	boolean check;
+	
+	String address1="/home/admin1/Desktop/UnorderedList.txt";			//address to create a file
+	 String address2="/home/admin1/Desktop/unorderedlistop.txt";
+	UtilityDataStructures utility=new UtilityDataStructures();			//object to invoke linkedlist object
+
+	String data=utility.llist.readfile(address1);						//method to read data from file
+	data=data.trim();													//removing the empty spaces
+	String[] input=data.split(" ");										//splitting the string into  words
+	for (int i = 0; i < input.length; i++) {
+		utility.llist.add(input[i]);									//adding words to linked list
 	}
+		utility.llist.printList();										//displaying linked list
+		System.out.println("enter the key to search");
+		String key=scan.nextLine();										//requesting for a word
+		check=utility.llist.search(key);								//method to check wethwe the word is present or not
+		
+		if(check==true)
+		{
+		utility.llist.delete(key);										//if it present it must be deleted
+			
+		}
+		else
+		{
+		utility.llist.add(key);											//if it not exist it must be added
+		}
+		
+		utility.llist.printList();						         		//displaying the final result
+		scan.close();
+		utility.llist.file(address2);
+		
 }
+
 }
+
